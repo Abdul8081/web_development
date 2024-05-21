c5c45c6d805025dac9ce56de7bdf72a81e0d7bd1
Abdul8081 <amuid677@gmail.com>
2024-05-21 16:29:39 +0530

    JS First class added

---
+++ JS_Basics_1/index.js
@@ -0,0 +1,86 @@
+
+
+//line 2 is describing here the print line 
+// console.log('Namaste Duniya version 4');
+
+// let a = 5;
+// console.log(a);
+
+// let status = true;
+// console.log(status);
+ 
+// var an = 10;
+// var an = 16;
+
+// const num = 12;
+// num = 13;
+// console.log(num);
+
+// let Lastname = 'babbar';
+// console.log(Lastname);
+
+// Lastname = 3;
+// console.log(Lastname);
+
+// console.log(2>3);
+
+ 
+// let num = 1;
+// let str = "1";
+
+// // console.log(num===str);
+// console.log(num==str);
+
+// let age = 27;
+// let votee = (age>=18) ? 'yes i can vote' : 'No vote';
+// console.log(votee);
+
+// let a = 5, b = 6;
+// console.log(a | b);
+
+// console.log(a & b);
+
+
+//else if
+// let marks = 98;
+// if(marks>=90){
+//     console.log('A');
+// }
+// else if(marks>=80){
+//     console.log('B');
+// }
+// else{
+//     console.log('F');
+// }
+
+//switch case
+// let num = 2;
+// switch(num){
+//     case 1 : console.log('a');
+//      break;
+//     case 2: console.log('b');
+//     break;
+//     case 3: console.log('c');
+//     break;
+//     default:console.log('d');
+// }
+
+//loops
+// for(let i = 0; i<5;i++){
+//     console.log('Abdul Muid' + " " + i);
+// }
+
+//while loop
+// let i = 0;
+// while(i<10){
+//     console.log(i);
+//     i++;
+// }
+
+//do while loop
+let y = 1;
+do{
+    console.log(y);
+    y++;
+}
+while(y<=10);
