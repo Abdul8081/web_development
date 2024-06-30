2bfc978bbb18f086854f74db1db20546d6c94681
Abdul8081 <amuid677@gmail.com>
2024-06-30 14:13:25 +0530

    React advance class 4 is added
    In this sir has completed the cart project and also in this , remining thing is card, in this componet, tailwind css is needed to apply, rest thing is working

---
+++ React_Advance/starter 4/src/redux/Slices/CartSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+export const CartSlice = createSlice({
+    name:"cart",
+    initialState:[],
+    reducers:{
+        add:(state,action) => {
+            // jo bhi action parameter me send kiya hota hai, wo action.payload se dikhaya jata hai
+           state.push(action.payload);
+        },
+        remove:(state,action) => {
+            return state.filter((item) => item.id !== action.payload);
+        },
+    }
+});
+
+export const {add, remove} = CartSlice.actions;
+export default CartSlice.reducer;
